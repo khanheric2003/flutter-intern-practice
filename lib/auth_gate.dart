4917121aa2b8aac6b8e2ceecf2b6af52e12e4bf4
khanheric <duykhanh.eric@gmail.com>
2024-05-15 06:01:41 +0700

    added temp profile and login

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+import 'package:flutter/material.dart';
 
 import 'home.dart';
 
@@ -14,15 +15,51 @@ class AuthGate extends StatelessWidget {
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return SignInScreen(
-              providers: [
-                EmailAuthProvider(),
-              ],
-              headerBuilder: (context, contraints, shrinkOffset) {
-                return const Padding(
-                  padding: EdgeInsets.all(20),
-                  child: AspectRatio(aspectRatio: 1, child: Text("someApp")),
-                );
-              });
+            providers: [
+              EmailAuthProvider(),
+              GoogleProvider(
+                  clientId:
+                      "1029381441351-in8nhr22phdqrmgdq3pl9tutuui618gp.apps.googleusercontent.com"),
+            ],
+            headerBuilder: (context, constraints, shrinkOffset) {
+              return const Padding(
+                padding: EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Icon(
+                    Icons.calendar_month_outlined,
+                    size: 80,
+                  ),
+                ),
+              );
+            },
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: action == AuthAction.signIn
+                    ? const Text('Welcome to Uremind, please sign in!')
+                    : const Text('Welcome to Uremind, please sign up!'),
+              );
+            },
+            footerBuilder: (context, action) {
+              return const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  'By signing in, you agree to our terms and conditions.',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              );
+            },
+            sideBuilder: (context, shrinkOffset) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset('assets/lutterfire_300x.png'),
+                ),
+              );
+            },
+          );
         }
 
         return const HomeScreen();
